7590bb49f5cf910c4856705275bbbccb500eff30
Tim Jacobi <dev@tim-jacobi.com>
2017-01-09 02:48:59 +0000

    add selection sort

---
+++ selection_sort.go
@@ -0,0 +1,23 @@
+package main
+
+import "fmt"
+
+func SelectionSort(input []int) []int {
+	for i :=0; i < len(input)-1; i++{
+		minIndex := i
+		for j:= minIndex; j < len(input); j++ {
+			if input[j] < input[minIndex]{
+				minIndex = j
+			}
+		}
+		temp := input[i]
+		input[i] = input[minIndex]
+		input[minIndex] = temp
+	}
+	return input
+}
+
+func main() {
+	list := []int{4,7,3,1,5,8,2,6}
+	fmt.Println(SelectionSort(list))
+}
