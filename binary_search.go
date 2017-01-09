ea2f2a68b333c30ad75601696c26f8fb20727b38
Tim Jacobi <dev@tim-jacobi.com>
2017-01-09 19:03:59 +0000

    add binary search

---
+++ binary_search.go
@@ -0,0 +1,31 @@
+package main
+
+import "fmt"
+
+func BinarySearchInner(input []int, value, start, end int) int {
+	if end-start == 0 {
+		return start
+	} else {
+		middle := ((end-start)/2)+start
+		if input[middle] == value {
+			return middle
+		} else if input[middle] > value {
+			return BinarySearchInner(input, value, start, middle-1)
+		} else {
+			return BinarySearchInner(input, value, middle+1, end)
+		}
+	}
+}
+
+func BinarySearch(input []int, value int) int {
+	return BinarySearchInner(input, value, 0, len(input)-1)
+}
+
+
+func main(){
+	list := []int{3, 26, 41, 57, 61, 63, 80, 93}
+
+	for _, value := range list {
+		fmt.Println(BinarySearch(list, value))
+	}
+}
