5c9fbe468cf158892f8c8312603a7d8430fcbe30
Tim Jacobi <info@tim-jacobi.com>
2017-01-14 17:39:48 +0530

    add comment to selection sort

--- selection_sort.go
+++ selection_sort.go
@@ -2,6 +2,12 @@ package main
 
 import "fmt"
 
+// Sorts the given input by iterating from the lowest
+// element up to second to last input element and comparing
+// it with all element that a right of the current element
+// stores the index of the smallest. If the smalles element
+// is not the current element it swaps the current element
+// with the smallest element.
 func SelectionSort(input []int) {
 	for i := 0; i < len(input)-1; i++ {
 		minIndex := i
