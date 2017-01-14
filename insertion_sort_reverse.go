8db9a043aa308d9cc0a59cf8bf304544a9f86fc3
Tim Jacobi <info@tim-jacobi.com>
2017-01-14 17:00:26 +0530

    add comment to insertion sort reverse

--- insertion_sort_reverse.go
+++ insertion_sort_reverse.go
@@ -2,6 +2,13 @@ package main
 
 import "fmt"
 
+// Sorts given input in order descending
+//
+// Iterates over elements 1 - n and shifts
+// all elements left of current element
+// that are smaller than the current element one
+// index to the right. Inserts current element
+// at index the last element was moved from.
 func InsertionSortReverse(input []int) {
 	for j := 1; j < len(input); j++ {
 		key := input[j]
