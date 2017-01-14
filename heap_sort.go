685f122ee512f19e552a3e50dce0e836a9535304
Tim Jacobi <info@tim-jacobi.com>
2017-01-14 16:56:04 +0530

    add comments to heap sort

--- heap_sort.go
+++ heap_sort.go
@@ -2,14 +2,29 @@ package main
 
 import "fmt"
 
+// Given an index for a node it returns the index of the node's left child
 func Left(i int) int {
 	return i*2 + 1
 }
 
+// Given an index for a node it returns the index of the node's right child
 func Right(i int) int {
 	return i*2 + 2
 }
 
+// Generates a max heap for root node in input at given index
+// Assumes children of node are roots of max heaps
+//
+// First compares value of node with value of left child and stores
+// the index of the larger of the two
+//
+// Then compares the value of the larger node to the value of the
+// right child and store the index of the larger of the two
+//
+// Finally if any of the children node's values was larger it swaps
+// the value of the child node with the value of the root node and
+// recurses on the child node to ensure the max heap property is
+// kept for the subtree
 func MaxHeapify(input []int, i int) {
 	var largest int
 	left := Left(i)
@@ -29,12 +44,25 @@ func MaxHeapify(input []int, i int) {
 	}
 }
 
+// Builds a max heap from the given input
+//
+// It iterates from the most left parent of the leaf nodes
+// (which start at |input|/2) up to the root and calls BuildMaxHeap
+// on each node
 func BuildMaxHeap(input []int) {
 	for i := len(input) / 2; i >= 0; i-- {
 		MaxHeapify(input, i)
 	}
 }
 
+// Sorts the given input
+//
+// First builds a max heap from the given input then
+// Continuously swaps the first and last element in the
+// heap, removes the last element from the heap and
+// restores the max heap property for the rest of the
+// heap until no more elements are left and the list
+// is sorted
 func HeapSort(input []int) {
 	BuildMaxHeap(input)
 	for i := len(input) - 1; i > 0; i-- {
