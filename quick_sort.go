1aef4a8709c53b23adc243bb4164b1d18dc1fa42
Tim Jacobi <info@tim-jacobi.com>
2017-01-14 17:35:52 +0530

    add comments to quick sort

--- quick_sort.go
+++ quick_sort.go
@@ -4,6 +4,12 @@ import (
 	"fmt"
 )
 
+// Partitions given input around a pivot element
+// so that all elements left of the pivot element
+// are smaller than the pivot element and all
+// elements right of the pivot element are larger
+// than the pivot element. Returns the index of
+// the pivot element.
 func Partition(input []int, p, r int) int {
 	i := p - 1
 	for j := p; j < r; j++ {
@@ -17,6 +23,12 @@ func Partition(input []int, p, r int) int {
 	return i + 1
 }
 
+// Sorts the given input in given boundaries
+//
+// First calculates a partition point using Partition
+// and then recurses on subarrays left and right of
+// partition point, i.e. [low boundary, partition point)
+// and (partition point, high boundary]
 func QuickSortInner(input []int, p, r int) {
 	if p < r {
 		q := Partition(input, p, r)
@@ -25,6 +37,7 @@ func QuickSortInner(input []int, p, r int) {
 	}
 }
 
+// Calculates the boundaries of input and kicks off the sorting
 func QuickSort(input []int) {
 	QuickSortInner(input, 0, len(input)-1)
 }
