165362b61c0819f03f2aabe4c9ee118d17239d77
Tim Jacobi <info@tim-jacobi.com>
2017-01-14 16:28:56 +0530

    add comments for binary search

--- binary_search.go
+++ binary_search.go
@@ -2,6 +2,17 @@ package main
 
 import "fmt"
 
+// Searches for a value in given input and start and end boundaries
+// Assumes input is sorted in ascending order
+//
+// Calculates the middle index using the start and end boundaries
+// If value at middle index is the value searched for it returns the middle index
+//
+// If value at middle index is lower than value searched for it recursively searches
+// input in [start, middle)
+//
+// If value at middle index is greater than value searched for it recursively searches
+// input in (middle, end]
 func BinarySearchInner(input []int, value, start, end int) int {
 	if start == end {
 		return start
@@ -17,6 +28,7 @@ func BinarySearchInner(input []int, value, start, end int) int {
 	}
 }
 
+// Calculates the boundaries of input and kicks off the binary search
 func BinarySearch(input []int, value int) int {
 	return BinarySearchInner(input, value, 0, len(input)-1)
 }
